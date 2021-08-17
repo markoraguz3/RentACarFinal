3bb738903f3b65a43ce1c1042f556cf36ad1abbe
IvanDodig <ivan.dodig.1998@gmail.com>
2021-08-17 10:23:11 +0200

    backend

--- Backend/Models/Cars.js
+++ Backend/Models/Cars.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const CarsSchema = mongoose.Schema({
-    // id: String,
-    seatsNum: {type: Number, min: 2 , max: 7, required: true},
-    priceDay: {type: Number, required: true},
-    fuelType: {type: String, required: true},
-    userId: String,
-})
+   seatsNum: { type: Number, min: 2, max: 7, required: true },
+   priceDay: { type: Number, required: true },
+   fuelType: { type: String, required: true },
+   images: { type: String, required: true },
+   userId: String,
+});
 
-module.exports = mongoose.model('Cars', CarsSchema);
+module.exports = mongoose.model("Cars", CarsSchema);
--- Backend/Routes/cars.js
+++ Backend/Routes/cars.js
@@ -1,73 +1,100 @@
 const express = require("express");
 const router = express.Router();
-const Car = require('../Models/Cars');
-const verify = require('./verifyToken');
+const verify = require("./verifyToken");
+const multer = require("multer");
+
+// File upload settings
+const upload = multer({
+   storage: multer.diskStorage({
+      destination: function (req, file, cb) {
+         cb(null, "./Uploads/");
+      },
+      filename: function (req, file, cb) {
+         cb(null, Date.now() + file.originalname);
+      },
+   }),
+   limits: {
+      fileSize: 1024 * 1024 * 5,
+   },
+   fileFilter: (req, file, cb) => {
+      if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+         cb(null, true);
+      } else {
+         cb(null, false);
+      }
+   },
+});
+const Car = require("../Models/Cars");
 
 // Get all cars
-router.get('/', async (req, res) => {
-    try {
-        const cars = await Car.find();
-        res.json(cars);
-    } catch(err) {
-        res.json({message: err.message});
-    }
+router.get("/", async (req, res) => {
+   try {
+      const cars = await Car.find();
+      res.json(cars);
+   } catch (err) {
+      res.json({ message: err.message });
+   }
 });
 
 // Get specific car
-router.get('/:id', async (req,res) => {
-    try {
-        const car = await Car.findById(req.params.postId);
-        res.json(car);
-    } catch(err) {
-        res.json({message: err.message});
-    }
+router.get("/:id", async (req, res) => {
+   try {
+      const car = await Car.findById(req.params.postId);
+      res.json(car);
+   } catch (err) {
+      res.json({ message: err.message });
+   }
 });
 
 // Create a new car
-router.post('/', verify, async (req, res) => {
-    const car = new Car({
-        seatsNum: req.body.seatsNum,
-        priceDay: req.body.priceDay,
-        fuelType: req.body.fuelType,
-        userId: req.body.userId,
-    })
+router.post("/", verify, upload.single("carImage"), async (req, res) => {
+   console.log(req.file);
+   const car = new Car({
+      seatsNum: req.body.seatsNum,
+      priceDay: req.body.priceDay,
+      fuelType: req.body.fuelType,
+      images: req.file.filename,
+      userId: req.body.userId,
+   });
 
-    try {
-        const savedCar = await car.save();
-        res.json(savedCar);
-    } catch(err) {
-        res.json({message: err.message});
-    }
+   console.log(car);
+   try {
+      const savedCar = await car.save();
+      res.json(savedCar);
+   } catch (err) {
+      res.json({ message: err.message });
+   }
 });
 
-// Update the car 
-router.patch('/:id', verify, async (req, res) => {
-    try {
-        const updatedCar = await Car.updateOne({_id: req.params}, 
-            {$set: 
-                { 
-                    seatsNum: req.body.seatsNum,
-                    priceDay: req.body.priceDay,
-                    fuelType: req.body.fuelType,
-                    userId: req.body.userId
-                }
-            });
+// Update the car
+router.patch("/:id", verify, async (req, res) => {
+   try {
+      const updatedCar = await Car.updateOne(
+         { _id: req.params },
+         {
+            $set: {
+               seatsNum: req.body.seatsNum,
+               priceDay: req.body.priceDay,
+               fuelType: req.body.fuelType,
+               userId: req.body.userId,
+            },
+         }
+      );
 
-        res.json(updatedCar);
-    
-    } catch (err) {
-        res.json({ message: err });
-    }
-})
+      res.json(updatedCar);
+   } catch (err) {
+      res.json({ message: err });
+   }
+});
 
-// Delete car 
-router.delete('/:id', verify,async (req, res) => {
-    try {
-        const removedCar = await Car.remove({_id: req.params.id});
-        res.json(removedCar);
-    } catch(err) {
-        res.json({message: err.message});
-    }
-})
+// Delete car
+router.delete("/:id", verify, async (req, res) => {
+   try {
+      const removedCar = await Car.remove({ _id: req.params.id });
+      res.json(removedCar);
+   } catch (err) {
+      res.json({ message: err.message });
+   }
+});
 
 module.exports = router;
